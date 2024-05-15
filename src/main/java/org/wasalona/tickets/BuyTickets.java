8262b44894cb1e3d4de31f77b297ec1fe0ca64a5
Alejandro Ajuria <alejoajuria@gmail.com>
2024-05-14 23:01:43 -0600

    Initial commit

---
+++ src/main/java/org/wasalona/tickets/BuyTickets.java
@@ -0,0 +1,93 @@
+package org.wasalona.tickets;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class BuyTickets {
+    public boolean buyTickets(int option, Player player) {
+        boolean hasMoney = hasAmount(player, option);
+
+        if(hasMoney) {
+            return giveTicket(player, option);
+        }
+
+        player.sendMessage(ChatColor.RED + "You don't have enough money to buy this ticket.");
+
+        return false;
+    }
+
+    private void executeCommand(String command) {
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+    }
+
+    private boolean giveTicket(Player player, int option) {
+        switch (option) {
+            case 1:
+                executeCommand("clear " + player.getName() + " lightmanscurrency:coin_gold 4");
+                executeCommand("scoreboard players add " + player.getName() + " mtr_balance 1");
+                return true;
+            case 2:
+                executeCommand("clear " + player.getName() + " lightmanscurrency:coin_gold 18");
+                executeCommand("scoreboard players add " + player.getName() + " mtr_balance 5");
+                return true;
+            case 3:
+                executeCommand("clear " + player.getName() + " lightmanscurrency:coin_gold 32");
+                executeCommand("scoreboard players add " + player.getName() + " mtr_balance 10");
+                return true;
+            case 4:
+                executeCommand("clear " + player.getName() + " lightmanscurrency:coin_gold 45");
+                executeCommand("scoreboard players add " + player.getName() + " mtr_balance 15");
+                return true;
+            case 5:
+                executeCommand("clear " + player.getName() + " lightmanscurrency:coin_diamond 1");
+                executeCommand("scoreboard players add " + player.getName() + " mtr_balance 30");
+                return true;
+            default:
+                player.sendMessage(ChatColor.RED + "Invalid option.");
+                return false;
+        }
+    }
+
+    private boolean hasAmount(Player player, int option) {
+        ItemStack[] items = player.getInventory().getContents();
+        boolean success = false;
+
+        switch (option) {
+            case 1:
+                for (ItemStack item : items) {
+                    if (item.getType() == Material.valueOf("LIGHTMANSCURRENCY_COIN_GOLD") && item.getAmount() >= 4) {
+                        success = true;
+                    }
+                }
+            case 2:
+                for (ItemStack item : items) {
+                    if (item.getType() == Material.valueOf("LIGHTMANSCURRENCY_COIN_GOLD") && item.getAmount() >= 18) {
+                        success = true;
+                    }
+                }
+            case 3:
+                for (ItemStack item : items) {
+                    if (item.getType() == Material.valueOf("LIGHTMANSCURRENCY_COIN_GOLD") && item.getAmount() >= 32) {
+                        success = true;
+                    }
+                }
+            case 4:
+                for (ItemStack item : items) {
+                    if (item.getType() == Material.valueOf("LIGHTMANSCURRENCY_COIN_GOLD") && item.getAmount() >= 45) {
+                        success = true;
+                    }
+                }
+            case 5:
+                for (ItemStack item : items) {
+                    if (item.getType() == Material.valueOf("LIGHTMANSCURRENCY_COIN_DIAMOND") && item.getAmount() >= 1) {
+                        success = true;
+                    }
+                }
+            default:
+                return success;
+        }
+    }
+}
---
+++ src/main/java/org/wasalona/tickets/Ticket.java
@@ -0,0 +1,101 @@
+package org.wasalona.tickets;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public final class Ticket extends JavaPlugin implements CommandExecutor, Listener {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("TicketsPlugin has been enabled!");
+
+        // Register command executor for /bounty command
+        Objects.requireNonNull(getCommand("ticket")).setExecutor(this);
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("BountiesPlugin has been disabled!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "You must be a player to use this command!");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if (args.length == 0) {
+            player.sendMessage(ChatColor.RED + "Usage: /ticket <option>");
+            return true;
+        }
+
+        // Handle command info
+        if (args[0].equalsIgnoreCase("info")) {
+            if (args.length != 1) {
+                player.sendMessage(ChatColor.RED + "Usage: /ticket info");
+                return true;
+            }
+
+            if (!player.hasPermission("tickets.info")) {
+                player.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
+                return true;
+            }
+
+            player.sendMessage("" + ChatColor.GOLD + ChatColor.BOLD +"Train Tickets");
+            player.sendMessage(ChatColor.AQUA + "1. " + ChatColor.GREEN + "1  Ticket  - " + ChatColor.GOLD + "4  Gold Coins");
+            player.sendMessage(ChatColor.AQUA + "2. " + ChatColor.GREEN + "5  Tickets - " + ChatColor.GOLD + "18 Gold Coins");
+            player.sendMessage(ChatColor.AQUA + "3. " + ChatColor.GREEN + "10 Tickets - " + ChatColor.GOLD + "32 Gold Coins");
+            player.sendMessage(ChatColor.AQUA + "4. " + ChatColor.GREEN + "15 Tickets - " + ChatColor.GOLD + "45 Gold Coins");
+            player.sendMessage(ChatColor.AQUA + "5. " + ChatColor.GREEN + "30 Tickets - " + ChatColor.AQUA + "1  Diamond Coin");
+            return true;
+        }
+
+        if (args.length != 1) {
+            player.sendMessage(ChatColor.RED + "Usage: /ticket <option>");
+            return true;
+        }
+
+
+        // Check permissions
+        if (!player.hasPermission("tickets.buy")) {
+            player.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
+            return true;
+        }
+
+        int option;
+        try {
+            option = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            option = 0;
+        }
+
+        // Handle command buy
+        if (option == 0) {
+            player.sendMessage(ChatColor.RED + "Invalid option.");
+            return true;
+        }
+
+        BuyTickets ticketHelper = new BuyTickets();
+
+        boolean success = ticketHelper.buyTickets(option, player);
+
+        if (success) {
+            player.sendMessage(ChatColor.GREEN + "You have successfully bought the tickets!");
+        } else {
+            player.sendMessage(ChatColor.RED + "There was an error!");
+        }
+
+        return true;
+    }
+}
